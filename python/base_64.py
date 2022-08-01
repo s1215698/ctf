ad60b2a48e32dfb74204a53ee09ade457df8b82a
王清熙 <s1215698@gmail.com>
2022-08-01 22:09:15 +0800

    init

---
+++ python/base_64.py
@@ -0,0 +1,21 @@
+#coding='utf-8'
+import base64
+import sys
+
+#choose = input("目標:\n1、Base64加密\n2、Base64解密\n")
+
+#if choose == '1':
+    #plain = input("輸入欲加密明文:\n")
+    #print ("\n加密後為:\n" + base64.b64encode(plain)).
+    #sys.exit()
+
+#elif choose == '2':
+cipher = input("輸入base64密文:\n") 
+print ("\n解密後為:\n"+ base64.b64decode(cipher).hex())
+    
+#else :
+    #print ("NO......")
+    #sys.exit()
+
+
+
